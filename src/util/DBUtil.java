4452ddf6e4b888885efc6cb498c364626b856e00
LAPTOP-C8D6IHE6\ysj3ssg <0515younsuji@naver.com>
2017-08-24 11:34:30 +0900

    younsuji_ver2

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -1,5 +1,61 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class DBUtil {
 
+	public static Connection getConnect() {
+		Connection conn = null;
+		String url = "jdbc:oracle:thin:@70.12.110.55:1521:xe";
+		String user = "yolowa";
+		String password = "yolowa";
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	public static void dbClose(Connection conn, Statement st, ResultSet rs) {
+		try {
+			if (rs != null)
+				rs.close();
+			if (st != null)
+				st.close();
+			if (conn != null)
+				conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static java.sql.Date stringToDate(String sdate) {
+		java.sql.Date d = null;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+
+		java.util.Date d2;
+		try {
+			d2 = sdf.parse(sdate);
+			d = convertDate(d2);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return d;
+	}
+
+	public static java.sql.Date convertDate(java.util.Date d) {
+		Date d2 = new Date(d.getTime());
+		return d2;
+	}
 }
